6f1ee409542c1dceffe5290f3643860891f564e8
despacito12 <31024960+despacito12@users.noreply.github.com>
2017-12-17 23:30:00 +0800

    Add files via upload

---
+++ cy/OperatingSystems-master/Scheduling/Scripts/app/algorithms/priority1.js
@@ -0,0 +1,23 @@
+(function (main) {
+    var name = "Non-preemptive Priority Scheduling (DESCENDING)"
+
+     function prio() {
+        var sorted = this.sort(function (x, y) {
+            var arrive1 = Math.abs(x.queueArrival() - ko.utils.unwrapObservable(main.algorithms.counters.processCounter)),
+                arrive2 = Math.abs(y.queueArrival() - ko.utils.unwrapObservable(main.algorithms.counters.processCounter));
+            if (x.priority() != y.priority()) {
+                return x.priority() <= y.priority() && arrive1 >= arrive2;
+            } else {
+                return x.queueArrival() >= y.queueArrival();
+            }
+        });
+        var inQueue = sorted.filter(function (i) { return i.lastProcessed > 0 })[0];
+
+        return {
+            nextJob: inQueue ? inQueue : sorted[0],
+            processedCollection: sorted
+        };
+    }
+
+    main.algorithms.add(name, prio);
+})(processScheduling);
